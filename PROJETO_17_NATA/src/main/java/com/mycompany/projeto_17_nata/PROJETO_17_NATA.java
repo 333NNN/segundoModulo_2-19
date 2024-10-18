7c0aa3d87193c898a47c69eaab0a35b8ca67328a
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-10-18 10:22:46 -0300

    Add files via upload

---
+++ PROJETO_17_NATA/src/main/java/com/mycompany/projeto_17_nata/PROJETO_17_NATA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_17_nata;
+
+/**
+ *
+ * @author Nata e Julia
+ */
+public class PROJETO_17_NATA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
