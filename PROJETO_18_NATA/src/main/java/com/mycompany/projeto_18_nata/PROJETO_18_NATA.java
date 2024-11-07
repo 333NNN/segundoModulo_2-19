f3dc799618c49804300fcf2ab47cd634d224aeb6
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-11-06 21:57:07 -0300

    Add files via upload

---
+++ PROJETO_18_NATA/src/main/java/com/mycompany/projeto_18_nata/PROJETO_18_NATA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_18_nata;
+
+/**
+ *
+ * @author ncunha
+ */
+public class PROJETO_18_NATA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
